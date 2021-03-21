c06f39b696bd4040ee8c2491dd9f1a204327dcaf
Ryota Mizumaki <elon.statham@gmail.com>
2021-03-21 15:37:23 +0900

    add typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
+const helloWorld = () => {
+    console.log("Hell World");
+};
+exports.helloWorld = helloWorld;
+exports.helloWorld();
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const helloWorld: () => void = () => {
+  console.log("Hell World");
+};
+
+helloWorld();
